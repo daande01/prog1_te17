1cd3b6a7010078316cc3fef8d02045303d3497b8
danand004 <danand004@5CG8312ZZ9.thn.local>
2018-10-15 10:32:12 +0200

    start

---
+++ prog1_te17/src/prog1_te17/Felsökning.java
@@ -0,0 +1,32 @@
+package prog1_te17;
+
+import java.util.Scanner;
+
+public class Felsökning {
+public static void main(String[] args) {
+	
+	Scanner input =new Scanner(System.in) ;
+			
+	System.out.print("tal A:");
+	int talA= input.nextInt();
+	
+	System.out.print("tal B:");
+	int talB= input.nextInt();
+	
+	System.out.print("tal C:");
+	int talC= input.nextInt();
+	
+	int summa = talA + talB + talC;
+	double medel = summa /3;
+	
+	
+	System.out.println("summa :"+summa);
+	System.out.println("medel:"+medel);
+	
+	
+	input.close();
+}
+	
+	
+	
+}
