5a7762d7c510687937287b7a5c0d39cff5e3070d
daande01 <daniel.evert.andersson@gmail.com>
2018-12-03 14:22:37 +0100

    metoder

--- prog1_te17/src/prog1_te17/metoder/Metoder_array1.java
+++ prog1_te17/src/prog1_te17/metoder/Metoder_array1.java
@@ -4,52 +4,39 @@ import java.util.Arrays;
 
 public class Metoder_array1 {
 
+	public static void changePosEttToHundra(int[] f) {
 
-	public static void changePosEttToHundra(int [] f) {
-
-
-		f[0]=100;
-
+		f[0] = 100;
 
 	}
 
+	public static double beräknaMedelVärde(int[] a) {
 
-
-
-	public static double beräknaMedelVärde(int [] a) {
-
-		int summa=0;
+		int summa = 0;
 
 		for (int i = 0; i < a.length; i++) {
 
-			 summa = summa+a[1];
-
+			summa = summa + a[1];
 
 		}
 
-		return summa/a.length;
+		return summa / a.length;
 	}
 
 	public static void main(String[] args) {
 
-		int [] a= new int [3];
+		int[] a = new int[3];
 
-		a[0]=1;
-		a[1]=2;
-		a[2]=3;
-
+		a[0] = 1;
+		a[1] = 2;
+		a[2] = 3;
 
 		System.out.println(beräknaMedelVärde(a));
-
 
 		changePosEttToHundra(a);
 
 		System.out.println(Arrays.toString(a));
 
-
-
-
 	}
-
 
 }
